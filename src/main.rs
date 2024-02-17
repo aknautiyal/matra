1e815add97f4a93767dd024d6b67a4db9545f960
Ankit Nautiyal <akn_github@outlook.com>
2024-02-17 14:24:56 +0530

    src/main: Update main to show vakya example
    Signed-off-by: Ankit Nautiyal <akn_github@outlook.com>

--- src/main.rs
+++ src/main.rs
@@ -9,48 +9,6 @@ use shabd::Shabd;
 use vakya::Vakya;
 
 fn main() {
-    let mut varnmala = Varnmala::new();
-
-    varnmala.initialize_varns();
-
-    varnmala.print_all_varns();
-
-    let mut varna: Varn;
-
-    let mut shabd = Shabd::new();
-    //let mut varnlist = VarnList::new();
-    varna = Varn::from_char('र');
-    //varnlist.push(varna);
-    shabd.base.push(varna);
-    varna = Varn::from_char('ा');
-    //varnlist.push(varna);
-    shabd.base.push(varna);
-    varna = Varn::from_char('ऽ');
-    shabd.base.push(varna);
-    varna = Varn::from_char('म');
-    //varnlist.push(varna);
-    shabd.base.push(varna);
-    varna = Varn::from_char('न');
-    shabd.base.push(varna);
-    varna = Varn::from_char('्');
-    shabd.base.push(varna);
-    varna = Varn::from_char('त');
-    shabd.base.push(varna);
-    varna = Varn::from_char('्');
-    shabd.base.push(varna);
-    varna = Varn::from_char('न');
-    shabd.base.push(varna);
-    varna = Varn::from_char('ा');
-    shabd.base.push(varna);
-    //varnlist.print_varns();
-    /*
-    if let Some(x) = varnlist.pop() {
-        x.print();
-    }
-    */
-    shabd.make_akshars();
-    shabd.print();
-
     let mut vakyas = Vakya::from_file("src/input.txt").unwrap();
     for vakya in vakyas.iter_mut() {
         vakya.print();
